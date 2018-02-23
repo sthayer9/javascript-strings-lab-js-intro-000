68e271038481250c4f02f03c11a03a9cba3bd861
Stephen Thayer <sthayer189@gmail.com>
2018-02-23 15:19:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ var specialGuest = "Hello, Neil deGrasse Tyson"
 
 var greetSpecialGuest = "" + specialGuest + "!";
 
-var topic = "Let's talk about space";
+var topic = "space";
 
-var conversation = `space`;
+var conversation = `Let's talk about space`;
